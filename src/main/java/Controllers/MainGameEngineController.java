217050e896b985cb6b029215d0320c1485a6e8a2
Anurag <anuragteckchandani@gmail.com>
2023-10-02 00:44:57 -0400

    Dependencies Added, Map Updated

--- src/main/java/Controllers/MainGameEngineController.java
+++ src/main/java/Controllers/MainGameEngineController.java
@@ -5,7 +5,6 @@ import Utils.CommandHandler;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Map;
 
 
 /**
--- src/main/java/Utils/CommandHandler.java
+++ src/main/java/Utils/CommandHandler.java
@@ -1,7 +1,4 @@
 package Utils;
-
-import Constants.GlobalApplicationConstants;
-
 import java.util.*;
 
 /**
@@ -21,7 +18,7 @@ public class CommandHandler {
      */
     public CommandHandler(final String p_command){
         final String l_trimLeadingAndTrailingSpaces = p_command.trim();
-        final String l_replaceAllMultipleWhiteSpacesWithSingleSpace = l_trimLeadingAndTrailingSpaces.replaceAll(" +", GlobalApplicationConstants.SPACE);
+        final String l_replaceAllMultipleWhiteSpacesWithSingleSpace = l_trimLeadingAndTrailingSpaces.replaceAll(" +", " ");
         this.d_terminalCommand = l_replaceAllMultipleWhiteSpacesWithSingleSpace;
     }
 
@@ -31,26 +28,26 @@ public class CommandHandler {
      * @return Root Command entered by the user
      */
     public String fetchRootCommand(){
-        return d_terminalCommand.split(GlobalApplicationConstants.SPACE)[0];
+        return d_terminalCommand.split(" ")[0];
     }
 
 
     public List<Map<String, String>> fetchAttemptedOperationAndPassedArguments(final String p_inputString){
         ArrayList<Map<String, String>> l_result = new ArrayList<>();
         final var l_mainRootCommand = fetchRootCommand();
-        var l_variousOperationArguments = l_mainRootCommand.replace(l_mainRootCommand,GlobalApplicationConstants.EMPTY_STRING).trim();
+        var l_variousOperationArguments = l_mainRootCommand.replace(l_mainRootCommand,"").trim();
 
         if(Objects.isNull(l_variousOperationArguments) || l_variousOperationArguments.isEmpty()){
             return l_result;
         }
 
-        final var l_isKeyWithNoHifenAndNoValuePart = !l_variousOperationArguments.contains(" ") && !l_variousOperationArguments.contains(GlobalApplicationConstants.SPACE);
+        final var l_isKeyWithNoHifenAndNoValuePart = !l_variousOperationArguments.contains(" ") && !l_variousOperationArguments.contains(" ");
 
         if(l_isKeyWithNoHifenAndNoValuePart){
-            l_variousOperationArguments = GlobalApplicationConstants.HIPHEN+GlobalApplicationConstants.FILENAME+GlobalApplicationConstants.SPACE + l_variousOperationArguments;
+            l_variousOperationArguments = "-filename "+ l_variousOperationArguments;
         }
 
-        var operationArguments = l_variousOperationArguments.split(GlobalApplicationConstants.HIPHEN);
+        var operationArguments = l_variousOperationArguments.split("-");
 
         for(String operationArgument : operationArguments){
             l_result.add(parseOperationArgument(operationArgument));
@@ -62,16 +59,16 @@ public class CommandHandler {
     private Map<String, String> parseOperationArgument(String p_operationArgument){
         Map<String, String> l_result = new HashMap<>();
 
-        var l_splitOperationArgument = p_operationArgument.split(GlobalApplicationConstants.SPACE);
+        var l_splitOperationArgument = p_operationArgument.split(" ");
 
-        l_result.put(GlobalApplicationConstants.KEY_OPERATION,l_splitOperationArgument[0]);
+        l_result.put("KEY_OPERATION",l_splitOperationArgument[0]);
         var l_argumentsParameters = "";
         if(l_splitOperationArgument.length > 1){
             String[] l_operationArgumentCombined = Arrays.copyOfRange(l_splitOperationArgument, 1, l_splitOperationArgument.length);
-            l_argumentsParameters = String.join( GlobalApplicationConstants.SPACE,l_operationArgumentCombined );
+            l_argumentsParameters = String.join(" ",l_operationArgumentCombined );
         }
 
-        l_result.put(GlobalApplicationConstants.KEY_OPERATION,l_argumentsParameters);
+        l_result.put("KEY_OPERATION",l_argumentsParameters);
 
         return l_result;
     }
