ca338b82cb348444fce23f3f8b2bf62a6a77fca0
abhishekhandacse <abhishekhandacse@gmail.com>
2023-10-02 22:25:21 -0400

    reset map logic

--- src/main/java/Controllers/MapController.java
+++ src/main/java/Controllers/MapController.java
@@ -3,4 +3,8 @@ package Controllers;
 public class MapController {
 
 
+    public void resetMap(GameState p_gameState) {
+        consoleLogger.writeLog("Map cannot be loaded, as it is invalid. Kindly provide valid map");
+        p_gameState.setD_map(new Models.Map());
+    }
 }
