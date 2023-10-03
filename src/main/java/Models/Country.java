00e295737e18454269fd0c7018fbf1e953fe9a18
amanpreet <batra2304@gmail.com>
2023-10-03 14:11:37 -0400

    Added Getter Setter Methods to the Country model class

--- src/main/java/Models/Country.java
+++ src/main/java/Models/Country.java
@@ -20,8 +20,44 @@ public class Country {
         d_continentId = p_continentId;
     }
 
+
     public Country(String p_countryName) {
         d_countryName = p_countryName;
     }
 
+    public void setD_armies(Integer p_armies) {
+        this.d_armies = p_armies;
+    }
+
+    public void setD_countryId(Integer p_countryId) {
+        this.d_countryId = p_countryId;
+    }
+
+    public void setD_continentId(Integer p_continentId) {
+        this.d_continentId = p_continentId;
+    }
+
+    public void setD_adjacentCountryIds(List<Integer> p_adjacentCountryIds) {
+        this.d_adjacentCountryIds = p_adjacentCountryIds;
+    }
+
+    public void setD_countryName(String p_countryName) {
+        this.d_countryName = p_countryName;
+    }
+
+    public Integer getD_countryId() {
+        return d_countryId;
+    }
+
+    public Integer getD_armies() {
+        return d_armies;
+    }
+
+    public Integer getD_continentId() {
+        return d_continentId;
+    }
+
+    public String getD_countryName() {
+        return d_countryName;
+    }
 }
