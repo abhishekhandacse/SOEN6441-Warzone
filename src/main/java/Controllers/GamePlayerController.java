9b246b50fcbaef3286a7032eb8035576fb22d682
Rajat Sharma <32574865+rajatjc@users.noreply.github.com>
2023-10-03 10:00:14 -0400

    Issue[#1] Method to find unique name added

--- src/main/java/Controllers/GamePlayerController.java
+++ src/main/java/Controllers/GamePlayerController.java
@@ -1,5 +1,26 @@
 package Controllers;
 
+
+import Logger.ConsoleLogger;
+import Models.*;
+import Utils.CommonUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class GamePlayerController {
-    
+    ConsoleLogger consoleLogger = new ConsoleLogger();
+	public boolean isPlayerNameUnique(List<Player> p_existingPlayerList, String p_playerName) {
+		boolean l_isUnique = true;
+		if (!CommonUtil.isCollectionEmpty(p_existingPlayerList)) {
+			for (Player l_player : p_existingPlayerList) {
+				if (l_player.getPlayerName().equalsIgnoreCase(p_playerName)) {
+					l_isUnique = false;
+					break;
+				}
+			}
+		}
+		return l_isUnique;
+	}
 }
