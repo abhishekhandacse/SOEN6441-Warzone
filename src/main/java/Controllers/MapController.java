7f424b669c7441630fdf926d5b6447308513bd6d
abhishekhandacse <abhishekhandacse@gmail.com>
2023-10-02 22:26:13 -0400

    Continent Metadata handled

--- src/main/java/Controllers/MapController.java
+++ src/main/java/Controllers/MapController.java
@@ -3,6 +3,15 @@ package Controllers;
 public class MapController {
 
 
+    private void writeContinentMetadata(GameState p_gameState, FileWriter p_writer) throws IOException {
+        p_writer.write(System.lineSeparator() + "[continents]" + System.lineSeparator());
+        for (Continent l_continent : p_gameState.getD_map().getD_continents()) {
+            p_writer.write(
+                    l_continent.getD_continentName().concat(" ").concat(l_continent.getD_continentValue().toString())
+                            + System.lineSeparator());
+        }
+    }
+
     public void resetMap(GameState p_gameState) {
         consoleLogger.writeLog("Map cannot be loaded, as it is invalid. Kindly provide valid map");
         p_gameState.setD_map(new Models.Map());
