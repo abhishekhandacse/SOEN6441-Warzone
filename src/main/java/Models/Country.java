6aa63a6be84156a47dfa571b3f9b04f9786897e6
amanpreet <batra2304@gmail.com>
2023-10-03 14:08:27 -0400

    Country Model defined and class constructors created

--- src/main/java/Models/Country.java
+++ src/main/java/Models/Country.java
@@ -1,5 +1,27 @@
 package Models;
 
+import Logger.ConsoleLogger;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Country {
-    
+
+    ConsoleLogger consoleLogger = new ConsoleLogger();
+    Integer d_armies;
+    Integer d_countryId;
+    Integer d_continentId;
+    String d_countryName;
+    List<Integer> d_adjacentCountryIds = new ArrayList<Integer>();;
+
+    public Country(int p_countryId, String p_countryName, int p_continentId) {
+        d_countryId = p_countryId;
+        d_countryName = p_countryName;
+        d_continentId = p_continentId;
+    }
+
+    public Country(String p_countryName) {
+        d_countryName = p_countryName;
+    }
+
 }
