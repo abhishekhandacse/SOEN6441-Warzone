3c474ad97594c9f9e2910a7423c67b10a2caaa20
amanpreet <batra2304@gmail.com>
2023-10-03 20:45:08 -0400

    Added new Method in  Command Tests to check command

--- src/test/java/Utils/CommandTest.java
+++ src/test/java/Utils/CommandTest.java
@@ -2,11 +2,22 @@ package Utils;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import static org.junit.Assert.*;
 
 public class CommandTest {
 
+    @Test
+    public void test_validCommand_getRootCommand(){
+        CommandHandler l_command = new CommandHandler("editcontinent -add continentID continentvalue");
+        String l_rootCommand = l_command.getRootCommand();
 
+        assertEquals("editcontinent",l_rootCommand);
+    }
 
 
 }
