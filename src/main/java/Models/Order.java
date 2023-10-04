4fd82ae275e67b1fafc1331e8a372dadbf42da96
coderjolly <harmanjolly97@gmail.com>
2023-10-03 21:23:51 -0400

    added Order Model setters

--- src/main/java/Models/Order.java
+++ src/main/java/Models/Order.java
@@ -6,4 +6,19 @@ public class Order {
 
     ConsoleLogger consoleLogger = new ConsoleLogger();
     
+    String d_orderAction;
+	String d_targetCountryName;
+	String d_sourceCountryName;
+	Integer d_numberOfArmiesToPlace;
+	Order orderObj;
+
+	
+	public Order() {
+	}
+
+	public Order(String p_orderAction, String p_targetCountryName, Integer p_numberOfArmiesToPlace) {
+		this.d_orderAction = p_orderAction;
+		this.d_targetCountryName = p_targetCountryName;
+		this.d_numberOfArmiesToPlace = p_numberOfArmiesToPlace;
+	}
 }
