e7f449383533ed59fb77e18fc0e2ca2d9aa76c0f
Anurag <anuragteckchandani@gmail.com>
2023-10-04 17:45:11 -0400

    JavaDoc for Continent Model

--- src/main/java/Models/Continent.java
+++ src/main/java/Models/Continent.java
@@ -6,24 +6,26 @@ import java.util.List;
 import Utils.CommonUtil;
 
 /**
- * The type Continent.
+ * Model Class for Continent
+ * 
+ * @author Anurag Teckchandani
  */
 public class Continent {
 
     /**
-     * The D continent value.
+     * d_continentValue: Signifies the value of continent ie the bonus value a player gets after conquering all countries in a continent.
      */
     Integer d_continentValue;
     /**
-     * The D countries.
+     * d_countries: List of countries.
      */
     List<Country> d_countries;
     /**
-     * The D continent id.
+     * d_continentID: Unique continent ID
      */
     Integer d_continentID;
     /**
-     * The D continent name.
+     * d_continentName: Continent Name
      */
     String d_continentName;
 
@@ -34,11 +36,11 @@ public class Continent {
     }
 
     /**
-     * Instantiates a new Continent.
+     * Parameterized Constructor
      *
-     * @param p_continentID    the p continent id
-     * @param p_continentName  the p continent name
-     * @param p_continentValue the p continent value
+     * @param p_continentID    the continent id
+     * @param p_continentName  the continent name
+     * @param p_continentValue the continent value
      */
     public Continent(Integer p_continentID, String p_continentName, int p_continentValue) {
         this.d_continentID = p_continentID;
@@ -47,81 +49,81 @@ public class Continent {
     }
 
     /**
-     * Instantiates a new Continent.
+     * Parameterized Constructor
      *
-     * @param p_continentName the p continent name
+     * @param p_continentName the continent name
      */
     public Continent(String p_continentName) {
         this.d_continentName = p_continentName;
     }
 
     /**
-     * Gets d continent id.
+     * Gets continent id.
      *
-     * @return the d continent id
+     * @return the continent id
      */
     public Integer getD_continentID() {
         return d_continentID;
     }
 
     /**
-     * Sets d continent id.
+     * Sets continent id.
      *
-     * @param p_continentID the p continent id
+     * @param p_continentID the continent id
      */
     public void setD_continentID(Integer p_continentID) {
         this.d_continentID = p_continentID;
     }
 
     /**
-     * Gets d continent name.
+     * Gets continent name.
      *
-     * @return the d continent name
+     * @return the continent name
      */
     public String getD_continentName() {
         return d_continentName;
     }
 
     /**
-     * Sets d continent name.
+     * Sets continent name.
      *
-     * @param p_continentName the p continent name
+     * @param p_continentName the continent name
      */
     public void setD_continentName(String p_continentName) {
         this.d_continentName = p_continentName;
     }
 
     /**
-     * Gets d continent value.
+     * Gets continent value.
      *
-     * @return the d continent value
+     * @return the continent value
      */
     public Integer getD_continentValue() {
         return d_continentValue;
     }
 
     /**
-     * Sets d continent value.
+     * Sets continent value.
      *
-     * @param p_continentValue the p continent value
+     * @param p_continentValue the continent value
      */
     public void setD_continentValue(Integer p_continentValue) {
         this.d_continentValue = p_continentValue;
     }
 
     /**
-     * Gets d countries.
+     * Gets countries.
      *
-     * @return the d countries
+     * @return the countries
      */
     public List<Country> getD_countries() {
         return d_countries;
     }
 
     /**
-     * Sets d countries.
+     * Sets countries.
      *
-     * @param p_countries the p countries
+     * @param p_countries the countries
      */
     public void setD_countries(List<Country> p_countries) {
         this.d_countries = p_countries;
@@ -130,7 +132,7 @@ public class Continent {
     /**
      * Remove all country neighbours.
      *
-     * @param p_countryId the p country id
+     * @param p_countryId the country id
      */
     public void removeAllCountryNeighbours(Integer p_countryId) {
         if (null != d_countries && !d_countries.isEmpty()) {
@@ -145,7 +147,7 @@ public class Continent {
     }
 
     /**
-     * Remove country.
+     * Remove country from map.
      *
      * @param p_country the p country
      */
@@ -158,9 +160,9 @@ public class Continent {
     }
 
     /**
-     * Add country.
+     * Add country to the map.
      *
-     * @param p_country the p country
+     * @param p_country the country
      */
     public void addCountry(Country p_country) {
         if (d_countries != null) {
