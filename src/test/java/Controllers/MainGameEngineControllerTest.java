d3ccbee70d8a53912d2b4c5b251580c2bfeb3a85
Anurag <anuragteckchandani@gmail.com>
2023-10-04 10:13:49 -0400

    Setup MainEngineController Test file

--- src/test/java/Controllers/MainGameEngineControllerTest.java
+++ src/test/java/Controllers/MainGameEngineControllerTest.java
@@ -1,4 +1,21 @@
 package Controllers;
 
+import org.junit.Before;
+
+import Models.Map;
+import Models.State;
+
 public class MainGameEngineControllerTest {
+
+    Map d_map;
+	State d_state;
+	MainGameEngineController d_engine;
+
+	
+	@Before
+	public void initialize() {
+		d_map = new Map();
+		d_engine = new MainGameEngineController();
+		d_state = d_engine.getD_state();
+	}
 }
