0ae6a3b801869d4ab7795450fde4178fca7f3ce5
abhishekhandacse <abhishekhandacse@gmail.com>
2023-10-29 23:22:02 -0400

    Various other hardcodings added

--- src/main/java/Constants/ApplicationConstantsHardcoding.java
+++ src/main/java/Constants/ApplicationConstantsHardcoding.java
@@ -15,4 +15,13 @@ public class ApplicationConstantsHardcoding {
     public static final String PURPLE_COLOR = "\033[0;35m";
     public static final String CYAN_COLOR = "\033[0;36m";
     public static final String WHITE_COLOR = "\u001B[47m";
+
+    public static final String ALL_CONTINENTS = "[continents]";
+    public static final String ALL_COUNTRIES = "[countries]";
+    public static final String ALL_BORDERS = "[borders]";
+    public static final String ALL_ARMIES = "Armies";
+    public static final String CONTINENT_CONTROL_VALUE = "Control Value";
+    public static final String GRAPH_CONNECTIONS = "Connections";
+    public static final String CLASSPATH_SRC_MAIN_RESOURCES = "src/main/resources";
+    public static final int DISPLAY_WIDTH = 80;
 }
