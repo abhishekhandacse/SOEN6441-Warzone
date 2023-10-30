7d7f1297a7a46714d85acbd88585216ebb28f404
abhishekhandacse <abhishekhandacse@gmail.com>
2023-10-29 23:19:22 -0400

    Colours Added

--- src/main/java/Constants/ApplicationConstantsHardcoding.java
+++ src/main/java/Constants/ApplicationConstantsHardcoding.java
@@ -1,4 +1,18 @@
 package Constants;
 
 public class ApplicationConstantsHardcoding {
+    public static final String VALID_MAP_MESSAGE = "The loaded map is valid!";
+
+    public static final String ARGUMENTS_PASSED = "arguments";
+    public static final String OPERATION_REQUESTED = "operation";
+
+    public static final String EXTENSION_MAP_FILE = ".map";
+
+    public static final String RED_COLOR = "\033[0;31m";
+    public static final String GREEN_COLOR = "\033[0;32m";
+    public static final String YELLOW_COLOR = "\033[0;33m";
+    public static final String BLUE_COLOR = "\033[0;34m";
+    public static final String PURPLE_COLOR = "\033[0;35m";
+    public static final String CYAN_COLOR = "\033[0;36m";
+    public static final String WHITE_COLOR = "\u001B[47m";
 }
