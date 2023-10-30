3047e5604e35df4d1060430a17d179b84bc7f174
abhishekhandacse <abhishekhandacse@gmail.com>
2023-10-29 23:26:22 -0400

    more hardcodings added

--- src/main/java/Constants/ApplicationConstantsHardcoding.java
+++ src/main/java/Constants/ApplicationConstantsHardcoding.java
@@ -1,5 +1,8 @@
 package Constants;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class ApplicationConstantsHardcoding {
     public static final String VALID_MAP_MESSAGE = "The loaded map is valid!";
 
@@ -24,4 +27,24 @@ public class ApplicationConstantsHardcoding {
     public static final String GRAPH_CONNECTIONS = "Connections";
     public static final String CLASSPATH_SRC_MAIN_RESOURCES = "src/main/resources";
     public static final int DISPLAY_WIDTH = 80;
+
+    public static final List<String> ALL_COLORS = Arrays.asList(RED_COLOR, GREEN_COLOR, YELLOW_COLOR, BLUE_COLOR, PURPLE_COLOR, CYAN_COLOR);
+    public static final List<String> BLOCKADEVALIDATION = Arrays.asList("bomb", "advance", "airlift", "negotiate");
+
+    public static final List<String> ALL_CARDS = Arrays.asList("bomb", "blockade", "airlift", "negotiate");
+    public static final int COLLECTION_SIZE = ALL_CARDS.size();
+
+    public static final String COMMAND_INVALID_ERROR_EDITMAP = "Invalid command. Kindly provide command in Format of : editmap filename";
+    public static final String COMMAND_INVALID_ERROR_EDITCONTINENT = "Invalid command. Kindly provide command in Format of : editcontinent -add continentID continentvalue -remove continentID";
+    public static final String COMMAND_INVALID_ERROR_EDITCOUNTRY = "Invalid command. Kindly provide command in Format of : editcountry -add countrytID continentID -remove countryID";
+    public static final String COMMAND_INVALID_ERROR_EDITNEIGHBOUR = "Invalid command. Kindly provide command in Format of : editneighbor -add countryID neighborcountryID -remove countryID neighborcountryID";
+    public static final String COMMAND_INVALID_ERROR_SAVEMAP = "Invalid command. Kindly provide command in Format of : savemap filename";
+
+    String VALIDATION_FAILD_MAP_ERROR_EMPTY = "No Map found! Please load a valid map to check!";
+    public static final String VALIDATION_FAILED_ERROR_LOADMAP = "Invalid command. Kindly provide command in Format of : loadmap filename";
+    public static final String VALIDATION_FAILED_ERROR_VALIDATEMAP = "Invalid command! validatemap is not supposed to have any arguments";
+    public static final String VALIDATION_FAILED_ERROR_GAMEPLAYER = "Invalid command. Kindly provide command in Format of : gameplayer -add playername -remove playername";
+    public static final String VALIDATION_FAILED_MAP_LOADED = "Map cannot be loaded, as it is invalid. Kindly provide valid map";
+    public static final String COMMAND_INVALID_ERROR_ASSIGNCOUNTRIES = "Invalid command. Kindly provide command in Format of : assigncountries";
+    public static final String COMMAND_INVALID_ERROR_DEPLOY_ORDER = "Invalid command. Kindly provide command in Format of : deploy countryID <CountryName> <num> (until all reinforcements have been placed)";
 }
