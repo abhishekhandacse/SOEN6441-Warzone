26b4aa28f22e48548c2a81cd75825692a69c40fc
Anurag <anuragteckchandani@gmail.com>
2023-11-03 00:48:46 -0400

    Added Card methods to Bomb

--- src/main/java/Models/Bomb.java
+++ src/main/java/Models/Bomb.java
@@ -1,6 +1,6 @@
 package Models;
 
-public class Bomb {
+public class Bomb implements Card{
     Player d_player;
 
 	String d_targetCountryID;
@@ -12,4 +12,41 @@ public class Bomb {
 		this.d_player = p_player;
 		this.d_targetCountryID = p_targetCountry;
 	}
+
+    @Override
+    public void execute(State p_state) {
+       
+    }
+
+    @Override
+    public boolean valid(State p_state) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'valid'");
+    }
+
+    @Override
+    public void printOrder() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'printOrder'");
+    }
+
+    @Override
+    public String orderExecutionLog() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'orderExecutionLog'");
+    }
+
+    @Override
+    public void setD_orderExecutionLog(String p_orderExecutionLog, String p_logType) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setD_orderExecutionLog'");
+    }
+
+    @Override
+    public String getOrderName() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getOrderName'");
+    }
+
+    
 }
