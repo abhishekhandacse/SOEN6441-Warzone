40dff86d9b72ab09847ca6b2429c8927f0a2be5c
coderjolly <harmanjolly97@gmail.com>
2023-11-04 15:48:46 -0400

    changed exception handling

--- src/main/java/Exceptions/MapValidationException.java
+++ src/main/java/Exceptions/MapValidationException.java
@@ -1,16 +1,7 @@
 package Exceptions;
 
-/**
- * The type Map validation exception.
- * 
- * @author Anurag Teckchandani
- */
 public class MapValidationException extends Exception {
-    /**
-     * Instantiates a new Map validation exception.
-     *
-     * @param p_message: Exception Message
-     */
+
     public MapValidationException(String p_message) {
         super(p_message);
     }
