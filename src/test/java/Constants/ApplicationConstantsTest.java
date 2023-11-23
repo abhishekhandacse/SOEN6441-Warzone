4037b00c427e565e7352bee971d020c744f75e59
Rajat Sharma <rajat.jc@gmail.com>
2023-11-22 23:56:23 -0500

    ApplicationContantsTest added content

--- src/test/java/Constants/ApplicationConstantsTest.java
+++ src/test/java/Constants/ApplicationConstantsTest.java
@@ -1,4 +1,9 @@
 package Constants;
 
+
 public class ApplicationConstantsTest {
+    // You can add test cases or assertions related to your application constants here.
+        // For example:
+    // public void testSomeConstant() { ... }
+    // public void testAnotherConstant() { ... }
 }
