595ec7f1176e7162826e6d4741ae2fafc6b39fab
Rajat Sharma <rajat.jc@gmail.com>
2023-11-22 23:42:24 -0500

    Model Bomb added javadoc added

--- src/main/java/Models/ModelBomb.java
+++ src/main/java/Models/ModelBomb.java
@@ -1,32 +1,53 @@
+/**
+ * The {@code ModelBomb} class represents a Bomb card in a game, allowing a player to execute a bomb order on a target country,
+ * reducing the number of armies on that country by half (rounded down) if the order is valid.
+ *
+ * @version 1.0
+ */
 package Models;
 
 import Utils.CommonUtil;
 
 import java.io.Serializable;
 
+/**
+ * Represents a Bomb card in the game.
+ * This card allows a player to execute a bomb order on a target country, reducing the number of armies by half if the order is valid.
+ */
 public class ModelBomb implements Card, Serializable {
 
-		ModelPlayer d_playerInitiator;
+	/** The player who initiated the bomb card. */
+	ModelPlayer d_playerInitiator;
 
+	/** The ID of the target country for the bomb card. */
+	String d_targetCountryID;
 
+	/** The log of order execution for the bomb card. */
+	String d_orderExecutionLog;
 
-
-		String d_targetCountryID;
-
-		String d_orderExecutionLog;
-
-		public ModelBomb(ModelPlayer p_playerInitiator, String p_targetCountry) {
+	/**
+	 * Constructs a new Bomb card with the specified initiator player and target country.
+	 *
+	 * @param p_playerInitiator The player who initiated the bomb card.
+	 * @param p_targetCountry   The ID of the target country for the bomb card.
+	 */
+	public ModelBomb(ModelPlayer p_playerInitiator, String p_targetCountry) {
 		this.d_playerInitiator = p_playerInitiator;
 		this.d_targetCountryID = p_targetCountry;
 	}
 
-		@Override
+	/**
+	 * Executes the bomb card, reducing the number of armies on the target country by half if the order is valid.
+	 *
+	 * @param p_gameState The current game state.
+	 */
+	@Override
 	public void execute(GameState p_gameState) {
 		if (checkValid(p_gameState)) {
 			Country l_targetCountry = p_gameState.getD_map().getCountryByName(d_targetCountryID);
 			int l_noOfArmiesOnTargetCountry = Math.max(1, l_targetCountry.getD_armies());
 			int l_newArmies = (int) Math.floor(l_noOfArmiesOnTargetCountry / 2);
-					l_targetCountry.setD_armies(l_newArmies);
+						l_targetCountry.setD_armies(l_newArmies);
 			d_playerInitiator.removeCard("bomb");
 			setD_orderExecutionLog(
 					"\nPlayer: " + this.d_playerInitiator.getPlayerName() + " is executing Bomb card on country: "
@@ -37,7 +58,13 @@ public class ModelBomb implements Card, Serializable {
 		}
 	}
 
-		@Override
+	/**
+	 * Checks if the bomb card order is valid.
+	 *
+	 * @param p_gameState The current game state.
+	 * @return {@code true} if the order is valid, {@code false} otherwise.
+	 */
+	@Override
 	public boolean checkValid(GameState p_gameState) {
 		Country l_country = d_playerInitiator.getD_coutriesOwned().stream()
 				.filter(l_pl -> l_pl.getD_countryName().equalsIgnoreCase(this.d_targetCountryID))
@@ -47,25 +74,33 @@ public class ModelBomb implements Card, Serializable {
 		if (!CommonUtil.isNull(l_country)) {
 			setD_orderExecutionLog(this.currentOrder() + " is not executed since Target country: "
 					+ this.d_targetCountryID + " given in bomb command is owned by the player: "
-						+ d_playerInitiator.getPlayerName() + " VALIDATES: You cannot bomb your own territory!", "error");
+								+ d_playerInitiator.getPlayerName() + " VALIDATES: You cannot bomb your own territory!", "error");
 			p_gameState.updateLog(orderExecutionLog(), "effect");
 			return false;
 		}
 
 		if (!d_playerInitiator.negotiationValidation(this.d_targetCountryID)) {
 			setD_orderExecutionLog(this.currentOrder() + " is not executed as " + d_playerInitiator.getPlayerName()
-					+ " has a negotiation pact with the target country's player!", "error");
+							+ " has a negotiation pact with the target country's player!", "error");
 			p_gameState.updateLog(orderExecutionLog(), "effect");
 			return false;
-			}
+		}
 		return true;
 	}
 
-		private String currentOrder() {
+	/**
+	 * Returns the current bomb card order.
+	 *
+	 * @return The current bomb card order.
+	 */
+	private String currentOrder() {
 		return "Bomb card order: " + "bomb" + " " + this.d_targetCountryID;
 	}
 
-		@Override
+	/**
+	 * Prints the bomb card order.
+	 */
+	@Override
 	public void printOrder() {
 		this.d_orderExecutionLog = "----------Bomb card order issued by player "
 				+ this.d_playerInitiator.getPlayerName() + "----------" + System.lineSeparator()
@@ -74,12 +109,23 @@ public class ModelBomb implements Card, Serializable {
 
 	}
 
-		public String orderExecutionLog() {
+	/**
+	 * Returns the order execution log for the bomb card.
+	 *
+	 * @return The order execution log for the bomb card.
+	 */
+	public String orderExecutionLog() {
 		return this.d_orderExecutionLog;
 	}
 
-		public void setD_orderExecutionLog(String p_orderExecutionLog, String p_logType) {
-				this.d_orderExecutionLog = p_orderExecutionLog;
+	/**
+	 * Sets the order execution log and prints it based on the log type.
+	 *
+	 * @param p_orderExecutionLog The order execution log.
+	 * @param p_logType           The type of the log ('error' or 'default').
+	 */
+	public void setD_orderExecutionLog(String p_orderExecutionLog, String p_logType) {
+		this.d_orderExecutionLog = p_orderExecutionLog;
 		if (p_logType.equals("error")) {
 			System.err.println(p_orderExecutionLog);
 		} else {
@@ -87,7 +133,13 @@ public class ModelBomb implements Card, Serializable {
 		}
 	}
 
-		@Override
+	/**
+	 * Checks if the bomb card order is valid in the given game state.
+	 *
+	 * @param p_currentGameState The current game state.
+	 * @return {@code true} if the order is valid, {@code false} otherwise.
+	 */
+	@Override
 	public Boolean checkIfOrderIsValid(GameState p_currentGameState) {
 		Country l_targetCountry = p_currentGameState.getD_map().getCountryByName(d_targetCountryID);
 		if (l_targetCountry == null) {
@@ -97,7 +149,12 @@ public class ModelBomb implements Card, Serializable {
 		return true;
 	}
 
-		@Override
+	/**
+	 * Returns the name of the bomb card order.
+	 *
+	 * @return The name of the bomb card order.
+	 */
+	@Override
 	public String getOrderName() {
 		return "bomb";
 	}
