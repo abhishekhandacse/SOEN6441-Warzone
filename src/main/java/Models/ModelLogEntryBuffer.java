6789edb0d82b9614f0bc469467745e5a2b3c761a
Rajat Sharma <rajat.jc@gmail.com>
2023-11-22 23:50:02 -0500

    Model Log Buffer javadoc added

--- src/main/java/Models/ModelLogEntryBuffer.java
+++ src/main/java/Models/ModelLogEntryBuffer.java
@@ -1,3 +1,8 @@
+/**
+ * The {@code ModelLogEntryBuffer} class represents a buffer for logging entries in a game.
+ *
+ * @version 1.0
+ */
 package Models;
 
 import Views.LogWriter;
@@ -5,20 +10,38 @@ import Views.LogWriter;
 import java.io.Serializable;
 import java.util.Observable;
 
+/**
+ * Represents a buffer for logging entries in a game.
+ */
 public class ModelLogEntryBuffer extends Observable implements Serializable {
 
-                    String d_logMessage;
+    /** The log message to be stored in the buffer. */
+                String d_logMessage;
 
-        public ModelLogEntryBuffer() {
+    /**
+     * Constructs a new log entry buffer and adds a {@code LogWriter} observer to handle log updates.
+     */
+    public ModelLogEntryBuffer() {
         LogWriter l_logWriter = new LogWriter();
         this.addObserver(l_logWriter);
     }
 
-        public String getD_logMessage() {
+    /**
+     * Gets the current log message.
+     *
+     * @return The current log message.
+     */
+    public String getD_logMessage() {
         return d_logMessage;
     }
 
-        public void currentLog(String p_messageToUpdate, String p_logType) {
+    /**
+     * Updates the log message based on the provided message and log type.
+     *
+     * @param p_messageToUpdate The message to be added to the log.
+     * @param p_logType         The type of log entry (command, order, phase, effect, start, end).
+     */
+    public void currentLog(String p_messageToUpdate, String p_logType) {
 
                   switch (p_logType.toLowerCase()) {
             case "command":
