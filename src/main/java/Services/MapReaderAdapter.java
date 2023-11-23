aaff2898063b13a670f8d96e049739f6a454b750
Rajat Sharma <rajat.jc@gmail.com>
2023-11-22 23:53:36 -0500

    Service map reader adapter javadoc added

--- src/main/java/Services/MapReaderAdapter.java
+++ src/main/java/Services/MapReaderAdapter.java
@@ -1,3 +1,8 @@
+/**
+ * The {@code MapReaderAdapter} class adapts the Conquest map file reader to the standard map file reader.
+ *
+ * @version 1.0
+ */
 package Services;
 
 import Models.GameState;
@@ -5,15 +10,31 @@ import Models.Map;
 
 import java.util.List;
 
+/**
+ * Adapts the Conquest map file reader to the standard map file reader.
+ */
 public class MapReaderAdapter extends MapFileReader {
 
-        private ConquestMapFileReader l_conquestMapFileReader;
+    /** The Conquest map file reader to be adapted. */
+    private ConquestMapFileReader l_conquestMapFileReader;
 
-        public MapReaderAdapter(ConquestMapFileReader p_conquestMapFileReader) {
+    /**
+     * Constructs a new map reader adapter with the given Conquest map file reader.
+     *
+     * @param p_conquestMapFileReader The Conquest map file reader to be adapted.
+     */
+    public MapReaderAdapter(ConquestMapFileReader p_conquestMapFileReader) {
         this.l_conquestMapFileReader = p_conquestMapFileReader;
     }
 
-        public void parseMapFile(GameState p_gameState, Map p_map, List<String> p_linesOfFile) {
+    /**
+     * Parses the map file using the adapted Conquest map file reader.
+     *
+     * @param p_gameState   The current game state.
+     * @param p_map         The game map.
+     * @param p_linesOfFile The lines of the map file.
+     */
+    public void parseMapFile(GameState p_gameState, Map p_map, List<String> p_linesOfFile) {
         l_conquestMapFileReader.readConquestFile(p_gameState, p_map, p_linesOfFile);
     }
 }
