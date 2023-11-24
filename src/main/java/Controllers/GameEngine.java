f6f0090788e28cc4460dc149511f7f300b71ca6a
Amanpreet <batra2304@gmail.com>
2023-11-23 23:44:01 -0500

    Changes in gameengine

--- src/main/java/Controllers/GameEngine.java
+++ src/main/java/Controllers/GameEngine.java
@@ -13,17 +13,25 @@ public class GameEngine {
      */
     private GameState d_stateOfGame = new GameState();
 
+    public GameState getD_gameState() {
+        return d_stateOfGame;
+    }
+
+    public void setD_gameState(GameState p_gameState) {
+        this.d_stateOfGame = p_gameState;
+    }
+
     /**
      * The present phase of the game.
      */
-    private Phase d_presentPhaseGame = new InitialStartUpPhase(this, d_stateOfGame);
+    private Phase d_presentPhaseGame = new initialStartUpPhase(this, d_stateOfGame);
 
     /**
      * Sets the current phase of the game.
      *
      * @param p_phase The new phase to set.
      */
-    private void setD_CurrentPhase(Phase p_phase) {
+    public void setD_CurrentPhase(Phase p_phase) {
         d_presentPhaseGame = p_phase;
     }
 
@@ -36,13 +44,36 @@ public class GameEngine {
         return d_presentPhaseGame;
     }
 
+    static boolean d_isTournamentMode = false;
+
+    public boolean isD_isTournamentMode() {
+        return d_isTournamentMode;
+    }
+
+    public void setD_isTournamentMode(boolean p_isTournamentMode) {
+        GameEngine.d_isTournamentMode = p_isTournamentMode;
+    }
+
+    public void loadPhase(Phase p_phase){
+        d_presentPhaseGame = p_phase;
+        d_stateOfGame = p_phase.getD_gameState();
+        getD_CurrentPhase().initPhase(d_isTournamentMode);
+    }
+
+    public void setStartUpPhase(){
+        this.setD_gameEngineLog("Start Up Phase", "phase");
+        setD_CurrentPhase(new initialStartUpPhase(this, d_stateOfGame));
+        getD_CurrentPhase().initPhase(d_isTournamentMode);
+    }
+
+
     /**
      * Sets the game to the Order Execution phase.
      */
     public void setOrderExecutionPhase() {
         this.setD_gameEngineLog("Execution of Order Phase", "phase");
         setD_CurrentPhase(new OrderExecutionPhase(this, d_stateOfGame));
-        getD_CurrentPhase().initPhase();
+        getD_CurrentPhase().initPhase(d_isTournamentMode);
     }
 
     /**
@@ -62,10 +93,10 @@ public class GameEngine {
     /**
      * Sets the game to the Issue Order phase.
      */
-    public void setIssueOrderPhase() {
+    public void setIssueOrderPhase(boolean p_isTournamentMode) {
         this.setD_gameEngineLog("Issue of Order Phase", "phase");
         setD_CurrentPhase(new IssueOrderPhase(this, d_stateOfGame));
-        getD_CurrentPhase().initPhase();
+        getD_CurrentPhase().initPhase(p_isTournamentMode);
     }
 
     /**
@@ -78,6 +109,6 @@ public class GameEngine {
 
         l_game.getD_CurrentPhase().getD_gameState().updateLog("Game is being initialized ....." + System.lineSeparator(), "start");
         l_game.setD_gameEngineLog("Startup of Game Phase", "phase");
-        l_game.getD_CurrentPhase().initPhase();
+        l_game.getD_CurrentPhase().initPhase(d_isTournamentMode);
     }
 }
