7ecad5bb9584f4ed91756b140e145a3afd7e5f1a
Amanpreet <batra2304@gmail.com>
2023-11-24 00:42:56 -0500

    Added new functionality to class MapWriterAdapter

--- src/main/java/Services/MapWriterAdapter.java
+++ src/main/java/Services/MapWriterAdapter.java
@@ -1,4 +1,21 @@
 package Services;
+import java.io.FileWriter;
+import java.io.IOException;
 
-public class MapWriterAdapter {
+import Models.GameState;
+
+
+public class MapWriterAdapter extends MapFileWriter{
+
+    private ConquestMapFileWriter l_conqMapFileWriter;
+
+    public MapWriterAdapter(ConquestMapFileWriter p_conqMapFileWriter) {
+        this.l_conqMapFileWriter = p_conqMapFileWriter;
+    }
+
+
+
+    public void parseMapToFile(GameState p_gameState, FileWriter p_writer, String p_mapFormat) throws IOException {
+        l_conqMapFileWriter.parseMapToFile(p_gameState, p_writer, p_mapFormat);
+    }
 }
