a5f2667cc04d2a0de91481796b74fef4f7954ec7
Amanpreet <batra2304@gmail.com>
2023-11-24 00:41:31 -0500

    Added javadoc to GameService

--- src/main/java/Services/GameService.java
+++ src/main/java/Services/GameService.java
@@ -5,8 +5,17 @@ import Models.Phase;
 
 import java.io.*;
 
+/**
+ * The GameService class load and save game file.
+ */
 public class GameService {
 
+    /**
+     * Serialize and save current game to specific file.
+     *
+     * @param p_phase instance of current game phase
+     * @param p_nameOfFile name of the file
+     */
     public static void saveGame(Phase p_phase, String p_nameOfFile){
         try {
             FileOutputStream l_gameSaveFile =new FileOutputStream(ApplicationConstants.SRC_MAIN_RESOURCES + "/" + p_nameOfFile);
@@ -19,6 +28,14 @@ public class GameService {
         }
     }
 
+    /**
+     * De-Serialize the Phase stored in specified file.
+     *
+     * @param p_nameOfFile name of file to load phase from
+     * @return the Phase saved in file
+     * @throws IOException input output exception when file not found
+     * @throws ClassNotFoundException if phase Phase class not found
+     */
     public static Phase loadGame(String p_nameOfFile) throws IOException, ClassNotFoundException {
         ObjectInputStream l_inpStream = new ObjectInputStream(new FileInputStream(ApplicationConstants.SRC_MAIN_RESOURCES + "/" + p_nameOfFile));
         Phase l_phase = (Phase) l_inpStream.readObject();
