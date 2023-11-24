5a68bf2da87d17a497974f020b54f2702551d2a3
Amanpreet <batra2304@gmail.com>
2023-11-23 23:52:58 -0500

    Added skeleton inhereted functions to CheaterPlayer model

--- src/main/java/Models/CheaterPlayer.java
+++ src/main/java/Models/CheaterPlayer.java
@@ -1,4 +1,44 @@
-package Models;
 
-public class CheaterPlayer {
+package Models;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import Services.PlayerService;
+
+public class CheaterPlayer extends PlayerBehaviorStrategy {
+
+
+    @Override
+    public String createOrder(ModelPlayer p_player, GameState p_stateOfGame) throws IOException {
+
+    }
+
+    @Override
+    public String createDeployOrder(ModelPlayer p_player, GameState p_gameState) {
+        return null;
+    }
+
+    @Override
+    public String createAdvanceOrder(ModelPlayer p_player, GameState p_gameState) {
+        return null;
+    }
+
+
+    @Override
+    public String createCardOrder(ModelPlayer p_player, GameState p_gameState, String p_cardName) {
+        return null;
+    }
+
+    @Override
+    public String getPlayerBehavior() {
+        return "Cheater";
+    }
+
+    private void updateContinents(ModelPlayer p_cheaterPlayer, ModelPlayer p_targetCPlayer,
+                                  GameState p_gameState) {
+
+    }
+
 }
