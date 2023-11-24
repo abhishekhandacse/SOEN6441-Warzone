38dc7ca3e49a07360fd0a68299c689b9fb2000df
Amanpreet <batra2304@gmail.com>
2023-11-24 17:45:57 -0500

    Added javadoc to MainGameTestSuite class

--- src/test/java/TestSuites/MainGameTestSuite.java
+++ src/test/java/TestSuites/MainGameTestSuite.java
@@ -6,7 +6,11 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-
+/**
+ * Test suite for testing issue and execution of order functionality and various
+ * player services of adding players, assigning armies and countries
+ *
+ */
 @RunWith(Suite.class)
 @SuiteClasses({ DeployTest.class, PlayerTest.class, PlayerServiceTest.class, AdvanceTest.class, AirliftTest.class,
 		BlockadeTest.class, ModelBombTest.class, DiplomacyTest.class, RandomPlayerTest.class, BenevolentPlayerTest.class, AggressivePlayerTest.class, TournamentTest.class})
