c3afbb3da9e01c4446c4d74b6195ebf2699b69dd
Anurag <anuragteckchandani@gmail.com>
2023-11-25 14:38:53 -0500

    Added unimplemented players HumanPlayer

--- src/main/java/Models/HumanPlayer.java
+++ src/main/java/Models/HumanPlayer.java
@@ -1,5 +1,37 @@
 package Models;
 
+import java.io.IOException;
+
 public class HumanPlayer extends PlayerBehaviorStrategy{
+
+    @Override
+    public String getPlayerBehavior() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getPlayerBehavior'");
+    }
+
+    @Override
+    public String createOrder(ModelPlayer p_modelPlayer, GameState p_currentGameState) throws IOException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createOrder'");
+    }
+
+    @Override
+    public String createCardOrder(ModelPlayer p_modelPlayer, GameState p_currentGameState, String p_currentCardName) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createCardOrder'");
+    }
+
+    @Override
+    public String createAdvanceOrder(ModelPlayer p_modelPlayer, GameState p_currentGameState) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createAdvanceOrder'");
+    }
+
+    @Override
+    public String createDeployOrder(ModelPlayer p_modelPlayer, GameState p_currentGameState) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createDeployOrder'");
+    }
     
 }
