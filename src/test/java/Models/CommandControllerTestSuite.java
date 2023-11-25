294586715ca80f4b78b839eda6057628d1d5371b
abhishekhandacse <abhishekhandacse@gmail.com>
2023-11-25 12:29:22 -0500

    Command Controller Test Suite

---
+++ src/test/java/Models/CommandControllerTestSuite.java
@@ -0,0 +1,17 @@
+package Models;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import Controllers.GameEngineUnitTest;
+import Models.OrderExecutionPhaseTest;
+import Utils.CommandTest;
+/**
+ * Test suite for testing command parsing utility and controller logic to check
+ * command validity
+ *
+ */
+@RunWith(Suite.class)
+@SuiteClasses({ CommandTest.class, GameEngineUnitTest.class, OrderExecutionPhaseTest.class })
+public class CommandControllerTestSuite {
+}
