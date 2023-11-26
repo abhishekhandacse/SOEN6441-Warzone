f9b6627057b3851edc8b159e10873b4d1e8b9dd2
coderjolly <harmanjolly97@gmail.com>
2023-11-26 02:56:32 -0500

    fixed CommandHandler.java with new changes

--- src/main/java/Utils/CommandHandler.java
+++ src/main/java/Utils/CommandHandler.java
@@ -1,29 +1,35 @@
 package Utils;
 
+import java.io.Serializable;
 import java.util.*;
-
 /**
- * The CommandHandler class is responsible for handling and parsing commands used in the game.
+ * A utility class for handling and parsing commands entered by the user.
+ * Provides methods to extract the root command, split commands into operations and arguments,
+ * and check for the presence of required keys in a command.
  */
 
-public class CommandHandler {
+public class CommandHandler implements Serializable {
 
-    /**
-     * the command
-     */
     public String d_command;
-
     /**
-     * Retrieves the operation and its associated arguments from the provided operation string.
+     * Constructs a new CommandHandler object with the specified command string.
      *
-     * @param p_operation The operation string to extract operation and arguments
-     * @return A map containing the extracted operation and its associated arguments
+     * @param p_command The command string to be processed.
+     */
+
+    public CommandHandler(String p_command){
+        this.d_command = p_command.trim().replaceAll(" +", " ");
+    }
+    /**
+     * Private helper method to create a map containing operation and arguments based on the given operation string.
+     *
+     * @param p_operation The operation string to be processed.
+     * @return A map containing the operation and its corresponding arguments.
      */
     private Map<String, String> getOperationAndArgumentsMap(String p_operation){
         Map<String, String> l_operationMap = new HashMap<String, String>();
         String[] l_split_operation = p_operation.split(" ");
         String l_arguments = "";
-
         l_operationMap.put("operation", l_split_operation[0]);
 
         if(l_split_operation.length > 1){
@@ -35,71 +41,54 @@ public class CommandHandler {
         return l_operationMap;
     }
 
-    /**
-     * Gets the root command from the provided command.
-     *
-     * @return The root command
-     */
-    public String getRootCommand(){
-        return d_command.split(" ")[0];
-    }
-
-    /**
-     * Retrieves the original command string.
-     *
-     * @return The original command string
-     */
     public String getD_command() {
         return d_command;
     }
 
-    /**
-     * Constructor for the CommandHandler class.
-     *
-     * @param p_command The command string to handle
-     */
-    public CommandHandler(String p_command){
-        this.d_command = p_command.trim().replaceAll(" +", " ");
+    public String getRootCommand(){
+        return d_command.split(" ")[0];
     }
-
     /**
-     * Gets a list of operations and their associated arguments from the command.
+     * Parses the original command string into a list of maps, each representing an operation and its arguments.
      *
-     * @return A list of maps containing operations and their arguments
+     * @return A list of maps containing operations and their corresponding arguments.
      */
     public List<Map<String , String>> getOperationsAndArguments(){
         String l_rootCommand = getRootCommand();
         String l_operationsString =  d_command.replace(l_rootCommand, "").trim();
+        
         if(null == l_operationsString || l_operationsString.isEmpty()) {
             return new ArrayList<Map<String , String>>();
         }
         boolean l_isFlagLessCommand = !l_operationsString.contains("-") && !l_operationsString.contains(" ");
 
+        // handle commands to load files, ex: loadmap filename
         if(l_isFlagLessCommand){
             l_operationsString = "-filename "+l_operationsString;
         }
+
         List<Map<String , String>> l_operations_list  = new ArrayList<Map<String,String>>();
         String[] l_operations = l_operationsString.split("-");
+
         Arrays.stream(l_operations).forEach((operation) -> {
             if(operation.length() > 1) {
                 l_operations_list.add(getOperationAndArgumentsMap(operation));
             }
         });
+
         return l_operations_list;
     }
-
     /**
-     * Checks if the required keys are present in the input map.
+     * Checks whether the required key is present in the provided map and has a non-empty value.
      *
-     * @param p_key      The key to check for in the input map
-     * @param p_inputMap The input map to check against
-     * @return True if the required key is present and not empty, false otherwise
+     * @param p_key      The key to be checked for presence.
+     * @param p_inputMap The map in which the presence of the key is checked.
+     * @return True if the key is present and has a non-empty value, otherwise false.
      */
     public boolean checkRequiredKeysPresent(String p_key, Map<String, String> p_inputMap) {
         if(p_inputMap.containsKey(p_key) && null != p_inputMap.get(p_key)
 				&& !p_inputMap.get(p_key).isEmpty())
-        return true;
-    return false;
+            return true;
+        return false;
     }
-
 }
