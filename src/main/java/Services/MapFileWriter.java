bd4a98c033161500bc93409ae6812863f9d53dab
coderjolly <harmanjolly97@gmail.com>
2023-11-26 02:47:27 -0500

    completed Javadocs for MapFileWriter.java

--- src/main/java/Services/MapFileWriter.java
+++ src/main/java/Services/MapFileWriter.java
@@ -12,7 +12,13 @@ import Models.Country;
 import Models.GameState;
 
 public class MapFileWriter implements Serializable {
-
+    /**
+     * Writes country metadata and organizes border data for each country to the specified file writer.
+     *
+     * @param p_gameState The game state containing map and country information.
+     * @param p_writer    The file writer to which the data will be written.
+     * @throws IOException If an I/O error occurs while writing to the file.
+     */
 	private void writeCountryAndBoarderMetaData(GameState p_gameState, FileWriter p_writer) throws IOException {
 		String l_countryMetaData = new String();
 		String l_bordersMetaData = new String();
@@ -42,8 +48,14 @@ public class MapFileWriter implements Serializable {
 				p_writer.write(l_borderStr + System.lineSeparator());
 			}
 		}
-	}
-
+	}  
+    /**
+     * Writes continent metadata to the specified file writer.
+     *
+     * @param p_gameState The game state containing map and continent information.
+     * @param p_writer    The file writer to which the data will be written.
+     * @throws IOException If an I/O error occurs while writing to the file.
+     */
 	private void writeContinentMetadata(GameState p_gameState, FileWriter p_writer) throws IOException {
 		p_writer.write(System.lineSeparator() + ApplicationConstants.CONTINENTS + System.lineSeparator());
 		for (Continent l_continent : p_gameState.getD_map().getD_continents()) {
@@ -52,7 +64,14 @@ public class MapFileWriter implements Serializable {
 							+ System.lineSeparator());
 		}
 	}
-
+    /**
+     * Parses the map information from the game state and writes it to the specified file using the provided file writer.
+     *
+     * @param p_gameState The game state containing map information.
+     * @param l_writer    The file writer to which the map information will be written.
+     * @param l_mapFormat The format in which the map information should be written.
+     * @throws IOException If an I/O error occurs while writing to the file.
+     */
 	public void parseMapToFile(GameState p_gameState, FileWriter l_writer, String l_mapFormat) throws IOException {
 		if (null != p_gameState.getD_map().getD_continents()
 				&& !p_gameState.getD_map().getD_continents().isEmpty()) {
