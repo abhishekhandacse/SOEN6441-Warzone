da02ef4f2f34f05d0bdad2fc6450ed7d4b812f59
coderjolly <harmanjolly97@gmail.com>
2023-11-26 03:21:23 -0500

    finalised tests for ModelPlayer.java, MapService.java

--- src/test/java/Models/ModelPlayerTest.java
+++ src/test/java/Models/ModelPlayerTest.java
@@ -12,101 +12,120 @@ import org.junit.Before;
 import org.junit.Test;
 
 /**
- * This class contains JUnit tests for the ModelPlayer class, which represents a player in a game.
+ *
+ * This class is used to test functionality of Player class functions.
+ *
  */
 public class ModelPlayerTest {
+	/**
+	 * Existing Player List.
+	 */
+	List<ModelPlayer> d_exisitingPlayerList = new ArrayList<>();
+	/**
+	 * Order List
+	 */
+	List<Order> d_order_list = new ArrayList<Order>();
 
-    List<ModelPlayer> d_exisitingPlayerList = new ArrayList<>();
-    List<Order> d_order_list = new ArrayList<Order>();
-    ModelPlayer d_player = new ModelPlayer();
-    GameState l_gs = new GameState();
+	/**
+	 * Player object
+	 */
+	ModelPlayer d_player = new ModelPlayer();
 
-    @Before
-    public void setup() {
-        d_exisitingPlayerList.add(new ModelPlayer("Anurag"));
-        d_exisitingPlayerList.add(new ModelPlayer("Rajat"));
+	/**
+	 * Current State of the Game.
+	 */
+	GameState l_gs = new GameState();
+	/**
+	 * The setup is called before each test case of this class is executed.
+	 */
+	@Before
+	public void setup() {
+		d_exisitingPlayerList.add(new ModelPlayer("Avneet"));
+		d_exisitingPlayerList.add(new ModelPlayer("Zalak"));
+		
+		Map l_map = new Map();
+		Country l_c1 = new Country(1, "Finland", 10);
+		l_c1.setD_adjacentCountryIds(Arrays.asList(2));
+		Country l_c2 = new Country(2, "France", 10);
+		List<Country> l_countryList = new ArrayList<>();
+		l_countryList.add(l_c1);
+		l_countryList.add(l_c2);
+		l_map.setD_countries(l_countryList);
+		l_gs.setD_map(l_map);
+	}
 
-        Map l_map = new Map();
-        Country l_c1 = new Country(1, "Finland", 10);
-        l_c1.setD_adjacentCountryIds(Arrays.asList(2));
-        Country l_c2 = new Country(2, "France", 10);
-        List<Country> l_countryList = new ArrayList<>();
-        l_countryList.add(l_c1);
-        l_countryList.add(l_c2);
-        l_map.setD_allCountries(l_countryList);
-        l_gs.setD_map(l_map);
-    }
+	/**
+	 * Tests first order in the players list of orders, then check if its removed
+	 * from the list.
+	 */
+	@Test
+	public void testNextOrder() {
 
-    /**
-     * Test for the next_order method, which retrieves the next order for a player.
-     */
-    @Test
-    public void testNextOrder() {
-        Order l_deployOrder1 = new ModelDeploy(d_exisitingPlayerList.get(0), "India", 5);
-        Order l_deployOrder2 = new ModelDeploy(d_exisitingPlayerList.get(1), "Finland", 6);
+		Order l_deployOrder1 = new ModelDeploy(d_exisitingPlayerList.get(0), "India", 5);
+		Order l_deployOrder2 = new ModelDeploy(d_exisitingPlayerList.get(1), "Finland", 6);
 
-        d_order_list.add(l_deployOrder1);
-        d_order_list.add(l_deployOrder2);
+		d_order_list.add(l_deployOrder1);
+		d_order_list.add(l_deployOrder2);
 
-        d_exisitingPlayerList.get(0).setD_ordersToExecute(d_order_list);
-        d_exisitingPlayerList.get(1).setD_ordersToExecute(d_order_list);
+		d_exisitingPlayerList.get(0).setD_ordersToExecute(d_order_list);
+		d_exisitingPlayerList.get(1).setD_ordersToExecute(d_order_list);
 
-        Order l_order = d_exisitingPlayerList.get(0).next_order();
-        assertEquals(l_deployOrder1, l_order);
-        assertEquals(1, d_exisitingPlayerList.get(0).getD_ordersToExecute().size());
-    }
+		Order l_order = d_exisitingPlayerList.get(0).next_order();
+		assertEquals(l_deployOrder1, l_order);
+		assertEquals(1, d_exisitingPlayerList.get(0).getD_ordersToExecute().size());
+	}
 
-    /**
-     * Test to validate a Deploy order with a specified number of armies.
-	 *  Required Test #5
-     */
-    @Test
-    public void testValidateDeployOrderArmies() {
-        d_player.setD_noOfUnallocatedArmies(10);
-        String l_noOfArmies = "4";
-        boolean l_bool = d_player.validateDeployOrderArmies(d_player, l_noOfArmies);
-        assertFalse(l_bool);
-    }
+	/**
+	 * Used to check that player cannot deploy more armies than there is in their
+	 * reinforcement pool.
+	 */
+	@Test
+	public void testValidateDeployOrderArmies() {
+		d_player.setD_noOfUnallocatedArmies(10);
+		String l_noOfArmies = "4";
+		boolean l_bool = d_player.validateDeployOrderArmies(d_player, l_noOfArmies);
+		assertFalse(l_bool);
+	}
 
-    /**
-     * Test for creating a Deploy order and validating the number of unallocated armies after the order is created.
-     */
-    @Test
-    public void testCreateDeployOrder() {
-        ModelPlayer l_pl = new ModelPlayer("Harman");
-        l_pl.setD_noOfUnallocatedArmies(20);
-        l_pl.createDeployOrder("Deploy India 5");
-        assertEquals(l_pl.getD_noOfUnallocatedArmies().toString(), "15");
-        assertEquals(l_pl.getD_ordersToExecute().size(), 1);
-    }
+	/**
+	 * Checks whether unassigned armies are updated after firing deploy order and
+	 * verifies unexecuted orders list.
+	 */
+	@Test
+	public void testCreateDeployOrder() {
+		ModelPlayer l_pl = new ModelPlayer("abc");
+		l_pl.setD_noOfUnallocatedArmies(20);
+		l_pl.createDeployOrder("Deploy India 5");
+		assertEquals(l_pl.getD_noOfUnallocatedArmies().toString(), "15");
+		assertEquals(l_pl.getD_ordersToExecute().size(), 1);
+	}
 
-    /**
-     * Test for checking if two countries are adjacent on the game map.
-     */
-    @Test
-    public void testCountryExists() {
-        ModelPlayer l_player = new ModelPlayer("Amanpreet");
-        assertTrue(l_player.checkAdjacency(l_gs, "Finland", "France"));
-        assertFalse(l_player.checkAdjacency(l_gs, "France", "Finland"));
-    }
-
-    /**
-     * Test for creating an Advance order and verifying the number of orders to execute.
-     */
-    @Test
-    public void testCreateAdvanceOrder() {
-        ModelPlayer l_player = new ModelPlayer("Rajat");
-        l_player.createAdvanceOrder("advance Finland France 10", l_gs);
-        assertEquals(l_player.getD_ordersToExecute().size(), 1);
-    }
-
-    /**
-     * Test for creating an Advance order with zero armies, which should result in no order being added.
-     */
-    @Test
-    public void testCreateAdvanceOrderFailure() {
-        ModelPlayer l_player = new ModelPlayer("Anurag");
-        l_player.createAdvanceOrder("advance Finland France 0", l_gs);
-        assertEquals(l_player.getD_ordersToExecute().size(), 0);
-    }
+	/**
+	 * Checks whether countries given in advance order are adjacent.
+	 */
+	@Test
+	public void testCountryExists() {		
+		ModelPlayer l_player = new ModelPlayer("abc");
+		assertTrue(l_player.checkAdjacency(l_gs, "Finland", "France"));
+		assertFalse(l_player.checkAdjacency(l_gs, "France", "Finland"));
+	}
+	
+	/**
+	 * Checks whether advance order given is added in player's queue or not.
+	 */
+	@Test
+	public void testCreateAdvanceOrder() {
+		ModelPlayer l_player = new ModelPlayer("xyz");
+		l_player.createAdvanceOrder("advance Finland France 10", l_gs);
+		assertEquals(l_player.getD_ordersToExecute().size(), 1);
+	}
+	/**
+	 * Checks whether advance order given is not added in player's queue as it is invalid.
+	 */
+	@Test
+	public void testCreateAdvanceOrderFailure() {
+		ModelPlayer l_player = new ModelPlayer("xyz");
+		l_player.createAdvanceOrder("advance Finland France 0", l_gs);
+		assertEquals(l_player.getD_ordersToExecute().size(), 0);
+	}
 }
--- src/test/java/Services/MapServiceTest.java
+++ src/test/java/Services/MapServiceTest.java
@@ -20,41 +20,76 @@ import java.util.List;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+/**
+ * This class is used to test functionality of MapService class functions.
+ */
 public class MapServiceTest {
 
-	MapService d_mapService;
+	/**
+	 * MapService reference to store its object.
+	 */
+	MapService d_mapservice;
 
+	/**
+	 * Map reference to store its object.
+	 */
 	Map d_map;
 
+	/**
+	 * GameState reference to store its object.
+	 */
 	GameState d_state;
 
+	/**
+	 * Setup before each MapService Operations
+	 * 
+	 * @throws MapValidationException Invalid map exception
+	 */
 	@Before
-
 	public void setup() throws MapValidationException {
-		d_mapService = new MapService();
+		d_mapservice = new MapService();
 		d_map = new Map();
 		d_state = new GameState();
-		d_map = d_mapService.loadMap(d_state, "europe.map");
+		d_map = d_mapservice.loadMap(d_state, "europe.map");
 	}
 
+	/**
+	 * This test case is used to test the functionality of EditMap function.
+	 *
+	 * @throws IOException throws IOException
+	 * @throws MapValidationException Invalid map exception
+	 */
 	@Test
 	public void testEditMap() throws IOException, MapValidationException {
-		d_mapService.editMap(d_state, "test.map");
-		File l_file = new File(CommonUtil.getAbsolutePathForFile("test.map"));
+		d_mapservice.editMap(d_state, "test.map");
+		File l_file = new File(CommonUtil.getMapFilePath("test.map"));
 
 		assertTrue(l_file.exists());
 	}
 
+	/**
+	 * tests addition of continent via editcontinent operation
+	 * @throws IOException Exceptions
+	 * @throws MapValidationException Exception
+	 * @throws CommandValidationException Exception
+	 */
 	@Test
 	public void testEditContinentAdd() throws IOException, MapValidationException, CommandValidationException {
 		d_state.setD_map(new Map());
-		Map l_updatedContinents = d_mapService.addRemoveContinents(d_state, d_state.getD_map(), "Add", "Asia 10");
+		Map l_updatedContinents = d_mapservice.addRemoveContinents(d_state, d_state.getD_map(), "Add", "Asia 10");
 
-		assertEquals(l_updatedContinents.getD_allContinents().size(), 1);
-		assertEquals(l_updatedContinents.getD_allContinents().get(0).getD_continentName(), "Asia");
-		assertEquals(l_updatedContinents.getD_allContinents().get(0).getD_continentValue().toString(), "10");
+		assertEquals(l_updatedContinents.getD_continents().size(), 1);
+		assertEquals(l_updatedContinents.getD_continents().get(0).getD_continentName(), "Asia");
+		assertEquals(l_updatedContinents.getD_continents().get(0).getD_continentValue().toString(), "10");
 	}
 
+	/**
+	 * tests removal of continent via editcontinent operation
+	 *
+	 * @throws IOException Exceptions
+	 * @throws MapValidationException Exception
+	 * @throws CommandValidationException Exception
+	 */
 	@Test
 	public void testEditContinentRemove() throws IOException, MapValidationException, CommandValidationException {
 		List<Continent> l_continents = new ArrayList<>();
@@ -72,15 +107,19 @@ public class MapServiceTest {
 		l_continents.add(l_c2);
 
 		Map l_map = new Map();
-		l_map.setD_allContinents(l_continents);
+		l_map.setD_continents(l_continents);
 		d_state.setD_map(l_map);
-		Map l_updatedContinents = d_mapService.addRemoveContinents(d_state, d_state.getD_map(), "Remove", "Asia");
+		Map l_updatedContinents = d_mapservice.addRemoveContinents(d_state, d_state.getD_map(), "Remove", "Asia");
 
-		assertEquals(l_updatedContinents.getD_allContinents().size(), 1);
-		assertEquals(l_updatedContinents.getD_allContinents().get(0).getD_continentName(), "Europe");
-		assertEquals(l_updatedContinents.getD_allContinents().get(0).getD_continentValue().toString(), "20");
+		assertEquals(l_updatedContinents.getD_continents().size(), 1);
+		assertEquals(l_updatedContinents.getD_continents().get(0).getD_continentName(), "Europe");
+		assertEquals(l_updatedContinents.getD_continents().get(0).getD_continentValue().toString(), "20");
 	}
 
+	/**
+	 * This test case is used to test functionality of Load map continent Id and
+	 * values.
+	 */
 	@Test
 	public void testContinentIdAndValues() {
 		List<Integer> l_actualContinentIdList = new ArrayList<Integer>();
@@ -92,7 +131,7 @@ public class MapServiceTest {
 		List<Integer> l_expectedContinentValueList = new ArrayList<Integer>();
 		l_expectedContinentValueList.addAll(Arrays.asList(5, 4, 5, 3));
 
-		for (Continent l_continent : d_map.getD_allContinents()) {
+		for (Continent l_continent : d_map.getD_continents()) {
 			l_actualContinentIdList.add(l_continent.getD_continentID());
 			l_actualContinentValueList.add(l_continent.getD_continentValue());
 		}
@@ -101,6 +140,10 @@ public class MapServiceTest {
 		assertEquals(l_expectedContinentValueList, l_actualContinentValueList);
 	}
 
+	/**
+	 * This test case is used to test functionality of Load map country Id and
+	 * neighbors.
+	 */
 	@Test
 	public void testCountryIdAndNeighbors() {
 		List<Integer> l_actualCountryIdList = new ArrayList<Integer>();
@@ -119,7 +162,7 @@ public class MapServiceTest {
 			}
 		};
 
-		for (Country l_country : d_map.getD_allCountries()) {
+		for (Country l_country : d_map.getD_countries()) {
 			ArrayList<Integer> l_neighbours = new ArrayList<Integer>();
 			l_actualCountryIdList.add(l_country.getD_countryId());
 			l_neighbours.addAll(l_country.getD_adjacentCountryIds());
@@ -130,49 +173,77 @@ public class MapServiceTest {
 		assertEquals(l_expectedCountryNeighbors, l_actualCountryNeighbors);
 	}
 
-
+	/**
+	 * Tests the savemap operation on an Invalid Map
+	 *
+	 *  @throws MapValidationException Exception
+	 */
 	@Test
 	public void testSaveMapValidationException() throws MapValidationException {
-		d_map.setD_inputMapFile("europe.map");
+		d_map.setD_mapFile("europe.map");
 		d_state.setD_map(d_map);
-		d_mapService.mapSave(d_state, "europe.map");
+		d_mapservice.saveMap(d_state, "europe.map");
 		assertEquals("Log: Couldn't save the changes in map file!"+System.lineSeparator(), d_state.getRecentLog());
 	}
 
+	/**
+	 * Tests the add country operation via editCountry
+	 * @throws IOException Exception
+	 * @throws MapValidationException Exception
+	 * @throws CommandValidationException Exception
+	 */
 	@Test
 	public void testEditCountryAdd() throws IOException, MapValidationException, CommandValidationException {
-		d_mapService.loadMap(d_state, "test.map");
-		d_mapService.editFunctions(d_state, "add", "China Asia", 2);
+		d_mapservice.loadMap(d_state, "test.map");
+		d_mapservice.editFunctions(d_state, "add", "China Asia", 2);
 
 		assertEquals(d_state.getD_map().getCountryByName("China").getD_countryName(), "China");
 	}
 
+	/**
+	 * Tests the Remove Country Operation via editcountry
+	 * @throws MapValidationException Exception
+	 * @throws CommandValidationException Exception
+	 * @throws IOException handles input output exception
+	 */
 	@Test
 	public void testEditCountryRemove() throws MapValidationException, IOException, CommandValidationException {
-		d_mapService.loadMap(d_state, "test.map");
-		d_mapService.editFunctions(d_state, "remove", "Ukraine", 2);
+		d_mapservice.loadMap(d_state, "test.map");
+		d_mapservice.editFunctions(d_state, "remove", "Ukraine", 2);
 		assertEquals("Log: Country: Ukraine does not exist!"+System.lineSeparator(), d_state.getRecentLog());
 	}
 
+	/**
+	 * Tests the add neighbor operation via editneighbor
+	 * @throws MapValidationException Exception
+	 * @throws IOException Exception
+	 * @throws CommandValidationException Exception
+	 */
 	@Test
 	public void testEditNeighborAdd() throws MapValidationException, IOException, CommandValidationException {
-		d_mapService.loadMap(d_state, "test.map");
-		d_mapService.editFunctions(d_state, "Northern-America 10", "add", 1 );
-		d_mapService.editFunctions(d_state, "add", "Canada Northern-America",  2);
-		d_mapService.editFunctions(d_state, "add","Alaska Northern-America", 2);
-		d_mapService.editFunctions(d_state, "add", "Canada Alaska", 3);
+		d_mapservice.loadMap(d_state, "test.map");
+		d_mapservice.editFunctions(d_state, "Northern-America 10", "add", 1 );
+		d_mapservice.editFunctions(d_state, "add", "Canada Northern-America",  2);
+		d_mapservice.editFunctions(d_state, "add","Alaska Northern-America", 2);
+		d_mapservice.editFunctions(d_state, "add", "Canada Alaska", 3);
 
 		assertEquals(d_state.getD_map().getCountryByName("Canada").getD_adjacentCountryIds().get(0), d_state.getD_map().getCountryByName("Alaska").getD_countryId());
 	}
 
+	/**
+	 * Tests the remove neighbor operation via editneighbor
+	 * @throws MapValidationException Exception
+	 * @throws IOException Exception
+	 * @throws CommandValidationException Exception
+	 */
 	@Test
 	public void testEditNeighborRemove() throws MapValidationException, IOException, CommandValidationException{
-		d_mapService.editMap(d_state, "testedit.map");
-		d_mapService.editFunctions(d_state, "Asia 9", "add",   1);
-		d_mapService.editFunctions(d_state, "add", "Maldives Asia", 2);
-		d_mapService.editFunctions(d_state, "add", "Singapore Asia", 2);
-		d_mapService.editFunctions(d_state, "add", "Singapore Maldives", 3);
-		d_mapService.editFunctions(d_state, "remove", "Maldives Singapore", 3);
+		d_mapservice.editMap(d_state, "testedit.map");
+		d_mapservice.editFunctions(d_state, "Asia 9", "add",   1);
+		d_mapservice.editFunctions(d_state, "add", "Maldives Asia", 2);
+		d_mapservice.editFunctions(d_state, "add", "Singapore Asia", 2);
+		d_mapservice.editFunctions(d_state, "add", "Singapore Maldives", 3);
+		d_mapservice.editFunctions(d_state, "remove", "Maldives Singapore", 3);
 		assertEquals("Log: No Such Neighbour Exists"+System.lineSeparator(), d_state.getRecentLog());
 	}
 }
